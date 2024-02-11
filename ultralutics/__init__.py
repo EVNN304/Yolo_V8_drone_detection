51f092af3e9980ebf95a67db2961654dc6dc9ebc
EVV <71592796+Egorvasechko@users.noreply.github.com>
2024-02-11 23:27:08 +0300

    Add files via upload

--- ultralutics/__init__.py
+++ ultralutics/__init__.py
@@ -1,12 +1,7 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = '8.0.212'
+from .bot_sort import BOTSORT
+from .byte_tracker import BYTETracker
+from .track import register_tracker
 
-from ultralytics.models import RTDETR, SAM, YOLO
-from ultralytics.models.fastsam import FastSAM
-from ultralytics.models.nas import NAS
-from ultralytics.utils import SETTINGS as settings
-from ultralytics.utils.checks import check_yolo as checks
-from ultralytics.utils.downloads import download
-
-__all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'FastSAM', 'RTDETR', 'checks', 'download', 'settings'
+__all__ = 'register_tracker', 'BOTSORT', 'BYTETracker'  # allow simpler import
